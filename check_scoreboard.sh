b774912c75ab4ae1cb17e1e0f3172369d21fd795
mklarz <mklarz@protonmail.com>
2023-12-11 15:37:12 +0100

    use python to fetch data instead

--- check_scoreboard.sh
+++ check_scoreboard.sh
@@ -3,29 +3,14 @@
 # Dirty quick script to track the NPST2023 scoreboard. Buy me a beer :))
 
 ## CONSTANTS
-BASE_URL="https://wiarnvnpsjysgqbwigrn.supabase.co"
-LOGIN_URL="$BASE_URL/auth/v1/token?grant_type=password"
-PROFILES_URL="$BASE_URL/rest/v1/profiles?select=*"
-SCOREBOARD_URL="$BASE_URL/rest/v1/scoreboard?select=*"
-SCOREBOARD_TEST_URL="$BASE_URL/rest/v1/scoreboard_test?select=*"
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-DATA_PATH="$SCRIPT_PATH/data"
 
 ## GOGO
 echo "Starting script..."
 cd $SCRIPT_PATH # dirty
 source "$SCRIPT_PATH/.env"
 
-# Download newest scoreboard
-curl -s "$SCOREBOARD_URL" -H "apikey: $DASS_APIKEY" -o "$DATA_PATH/scoreboard.min.json"
-curl -s "$SCOREBOARD_TEST_URL" -H "apikey: $DASS_APIKEY" -o "$DATA_PATH/scoreboard_test.min.json"
-# Download profiles
-curl -s "$PROFILES_URL" -H "apikey: $DASS_APIKEY" -o "$DATA_PATH/profiles.min.json"
-
-# Let's "prettify" the json as well
-cat "$DATA_PATH/scoreboard.min.json" | jq > "$DATA_PATH/scoreboard.json"
-cat "$DATA_PATH/scoreboard_test.min.json" | jq > "$DATA_PATH/scoreboard_test.json"
-cat "$DATA_PATH/profiles.min.json" | jq > "$DATA_PATH/profiles.json"
+python3 ./fetch_data.py $DASS_APIKEY
 
 if [[ `git status --porcelain` ]]; then
   echo "There are differences, updating"
---
+++ fetch_data.py
@@ -0,0 +1,48 @@
+import sys
+import json
+import requests
+from pathlib import Path
+from pprint import pprint
+
+BASE_URL = "https://wiarnvnpsjysgqbwigrn.supabase.co"
+LOGIN_URL = f"{BASE_URL}/auth/v1/token?grant_type=password"
+PROFILES_URL = f"{BASE_URL}/rest/v1/profiles?select=*"
+SCOREBOARD_URL = f"{BASE_URL}/rest/v1/scoreboard?select=*"
+SCOREBOARD_TEST_URL = f"{BASE_URL}/rest/v1/scoreboard_test?select=*"
+PAGINATION_LIMIT = 2000
+DASS_APIKEY = sys.argv[1]
+
+CWD = Path.cwd()
+DATA_PATH = CWD / "data"
+
+s = requests.Session()
+s.headers["apikey"] = DASS_APIKEY
+
+def fetch_paginated_data(url):
+    data = []
+    offset = 0
+    while True:
+        params = {
+            "select": "*",
+            "limit": PAGINATION_LIMIT,
+            "offset": offset,
+        }
+        current_data = s.get(url, params=params).json()
+        data += current_data
+        if PAGINATION_LIMIT > len(current_data):
+            break
+        offset += PAGINATION_LIMIT
+    return data
+
+profiles = fetch_paginated_data(PROFILES_URL)
+scoreboard = fetch_paginated_data(SCOREBOARD_URL)
+
+with open(DATA_PATH / "profiles.min.json", "w") as fd:
+    json.dump(profiles, fd)
+with open(DATA_PATH / "profiles.json", "w") as fd:
+    json.dump(profiles, fd, indent=4)
+
+with open(DATA_PATH / "scoreboard.min.json", "w") as fd:
+    json.dump(scoreboard, fd)
+with open(DATA_PATH / "scoreboard.json", "w") as fd:
+    json.dump(scoreboard, fd, indent=4)
